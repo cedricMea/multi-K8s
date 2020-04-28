dfe3f97248d52b09713666dd07761c00f2581d61
cedricmea <meacedric@yahoo.fr>
2020-04-28 23:25:58 +0200

    Add deploy file

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+# 7- Build all images tag each one and push each one to docker hub
+
+# Build images
+docker build -t meacedric/multi-client:latest meacedric/multi-client:$SHA  -f ./client/Dockerfile ./client
+docker build -t meacedric/multi-server:latest meacedric/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t meacedric/multi-worker:latest meacedric/multi-worker:$SHA-f ./worker/Dockerfile ./worker
+
+#Push them to dockerHub
+docker push meacedric/multi-client:latest
+docker push meacedric/multi-server:latest
+docker push meacedric/multi-worker:latest
+
+docker push meacedric/multi-client:$SHA
+docker push meacedric/multi-server:$SHA
+docker push meacedric/multi-worker:$SHA
+
+
+# 8- Apply all configs in the k8s dir
+kubectl apply -f k8s
+
+# 9- Imperatively set latest (the one we just build referenced by commit id) image on each deployment
+kubectl set image deployments/server-deployement server=meacedric/multi-server:$SHA
+kubectl set image deployments/client-deployement client=meacedric/multi-client:$SHA
+kubectl set image deployments/worker-deployement worker=meacedric/multi-worker:$SHA
